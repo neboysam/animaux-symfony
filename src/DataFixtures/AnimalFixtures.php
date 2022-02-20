00547e41af6a2524f8ea71431a197e599a728076
neboysam <monte.nemi@gmail.com>
2022-02-20 12:22:34 +0100

    update

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,7 +3,9 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use App\Entity\Dispose;
 use App\Entity\Famille;
+use App\Entity\Personne;
 use App\Entity\Continent;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -14,6 +16,15 @@ class AnimalFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        $p1 = new Personne();
+        $p1->setNom('Milo');
+        $manager->persist($p1);
+        $p2 = new Personne();
+        $p2->setNom('Tya');
+        $manager->persist($p2);
+        $p3 = new Personne();
+        $p3->setNom('Leo');
+        $manager->persist($p3);
 
         $c1 = new Continent();
         $c1->setLibelle('Europe');
@@ -49,7 +60,7 @@ class AnimalFixtures extends Fixture
         $a1 = new Animal();
         $a1->setNom('Chien')
             ->setDescription('Un animal domestique')
-            ->setImage('chien.png')
+            ->setImage('dog.png')
             ->setPoids(10)
             ->setDangereux(false)
             ->setFamille($f1)
@@ -64,7 +75,7 @@ class AnimalFixtures extends Fixture
         $a2 = new Animal();
         $a2->setNom('Cochon')
             ->setDescription('Un animal d\'elevage')
-            ->setImage('cochon.png')
+            ->setImage('pig.png')
             ->setPoids(30)
             ->setDangereux(false)
             ->setFamille($f1)
@@ -79,7 +90,7 @@ class AnimalFixtures extends Fixture
         $a3 = new Animal();
         $a3->setNom('Serpent')
             ->setDescription('Un animal dangereux')
-            ->setImage('serpent.png')
+            ->setImage('Snake.png')
             ->setPoids(5)
             ->setDangereux(true)
             ->setFamille($f2)
@@ -103,10 +114,10 @@ class AnimalFixtures extends Fixture
         ;
         $manager->persist($a4);
 
-        $a4 = new Animal();
-        $a4->setNom('Requin')
-            ->setDescription('Un animal marin tres dangereux')
-            ->setImage('requin.png')
+        $a5 = new Animal();
+        $a5->setNom('Narval')
+            ->setDescription('Un animal marin bienveillant')
+            ->setImage('narwhal.png')
             ->setPoids(200)
             ->setDangereux(true)
             ->setFamille($f3)
@@ -114,7 +125,49 @@ class AnimalFixtures extends Fixture
             ->addContinent($c4)
             ->addContinent($c5)
         ;
-        $manager->persist($a4);
+        $manager->persist($a5);
+
+        $d1 = new Dispose();
+        $d1->setPersonne($p1)
+            ->setAnimal($a1)
+            ->setNb(5);
+        $manager->persist($d1);
+
+        $d2 = new Dispose();
+        $d2->setPersonne($p1)
+            ->setAnimal($a3)
+            ->setNb(4);
+        $manager->persist($d2);
+
+        $d3 = new Dispose();
+        $d3->setPersonne($p1)
+            ->setAnimal($a5)
+            ->setNb(3);
+        $manager->persist($d3);
+
+        $d4 = new Dispose();
+        $d4->setPersonne($p2)
+            ->setAnimal($a2)
+            ->setNb(6);
+        $manager->persist($d4);
+
+        $d5 = new Dispose();
+        $d5->setPersonne($p2)
+            ->setAnimal($a4)
+            ->setNb(8);
+        $manager->persist($d5);
+
+        $d6 = new Dispose();
+        $d6->setPersonne($p3)
+            ->setAnimal($a2)
+            ->setNb(8);
+        $manager->persist($d6);
+
+        $d7 = new Dispose();
+        $d7->setPersonne($p3)
+            ->setAnimal($a5)
+            ->setNb(7);
+        $manager->persist($d7);
 
         $manager->flush();
     }
